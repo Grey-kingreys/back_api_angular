20770863c503ccfaf4be0ba78a8ace5106848cdf
unknown <soulmamoudou0@gmail.com>
2025-12-27 01:24:20 +0000

    Test 1 apres ajout du cd/ci

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   })
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log('🚀 Backend running on http://localhost:3000');
+  console.log('🚀 Backend running on ');
 }
 bootstrap();
