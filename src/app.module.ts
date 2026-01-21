3411762a334c10a6bff0788da4918fc2b8480141
unknown <soulmamoudou0@gmail.com>
2026-01-21 09:33:40 +0000

    Ajout pour corriger l'heure de password

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { WeatherModule } from './weather/weather.module';
 import { ConfigModule } from '@nestjs/config';
 import { CountriesModule } from './countries/countries.module';
 import { UserModule } from './user/user.module';
-import { PrismaService } from './common/services/prisma.service';
 
 @Module({
   imports: [
@@ -15,6 +14,6 @@ import { PrismaService } from './common/services/prisma.service';
     UserModule
   ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
